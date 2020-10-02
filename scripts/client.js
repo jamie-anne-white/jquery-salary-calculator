d47636e66519e45d0840f83b2f8dfeb5c1d16427
Jamie Anne White <jamiewhite@Jamies-MBP.zyxel.com>
2020-10-02 18:09:27 -0500

    click event working

--- scripts/client.js
+++ scripts/client.js
@@ -1,10 +1,50 @@
 console.log('js');
+const employees = [{
+        FirstName: 'Leslie',
+        LastName: 'Knope',
+        employeeID: '1234',
+        employeeTitle: 'Deputy Directory',
+        annualSalary: '65,0000'
+    },
+    {
+        FirstName: 'Ron',
+        LastName: 'Swanson',
+        employeeID: '4567',
+        employeeTitle: 'Director',
+        annualSalary: '43,000'
+    },
+    {
+        FirstName: 'April',
+        LastName: 'Ludgate',
+        employeeID: '6666',
+        employeeTitle: 'Intern',
+        annualSalary: '22,000'
+    },
+    {
+        FirstName: 'Ann',
+        LastName: 'Perkins',
+        employeeID: '7890',
+        employeeTitle: 'Nurse',
+        annualSalary: '0'
+    }
+
+
+];
 
 $(document).ready(readyNow);
 
 function readyNow() {
     console.log('jquery')
     //works!
+     //click event 
+     $('#submit-button').on('click', addEmployee);
+     
+     
 
 
+}
+
+function addEmployee(){
+    console.log('click in addEmployee');
+    
 }
